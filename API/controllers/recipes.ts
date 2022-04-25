57340202fa52245afbe490e987f69cfb7bec971b
Joona Ruutiainen <ruutiainenjoona@gmail.com>
2022-04-25 19:18:42 +0300

    prevent changing recipe.userId with PUT /recipes/:recipeId

--- API/controllers/recipes.ts
+++ API/controllers/recipes.ts
@@ -77,8 +77,8 @@ const updateRecipe = async (req: Request, res: Response) => {
   try {
     const recipe = await validateRecipeId(req);
 
-    // recipe.public is not allowed to be modified with update recipe request
-    const { public: recipeIsPublic, ...dataToUpdate } = req.body;
+    // recipe.public and recipe.userId are not allowed to be modified with update recipe request
+    const { public: recipeIsPublic, userId, ...dataToUpdate } = req.body;
     let updatedRecipe = { ...recipe.toJSON(), ...dataToUpdate };
 
     await Recipe.validateRecipeData(updatedRecipe);
